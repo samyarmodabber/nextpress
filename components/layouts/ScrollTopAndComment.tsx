10228b2fe9b5aeac856b787e8533654c7c7efbf4
samyar modaber <samyar.modabber@gmail.com>
2024-07-19 13:17:34 +0200

    fix @emotion/react

--- components/layouts/ScrollTopAndComment.tsx
+++ components/layouts/ScrollTopAndComment.tsx
@@ -26,7 +26,7 @@ const ScrollTopAndComment = () => {
     <div
       className={`fixed bottom-8 right-8 hidden flex-col gap-3 ${show ? 'md:flex' : 'md:hidden'}`}
     >
-      {siteMetadata.comments.active && (
+      {siteMetadata.comments && (
         <button
           aria-label="Scroll To Comment"
           onClick={handleScrollToComment}

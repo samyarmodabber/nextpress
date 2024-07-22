9268737195badc3b6b6ea0a0b5e2d8654a1abf6a
samyar modaber <samyar.modabber@gmail.com>
2024-07-22 13:14:41 +0200

    fix TOCInline

--- components/markdown/MDXComponents.tsx
+++ components/markdown/MDXComponents.tsx
@@ -1,7 +1,9 @@
-import TOCInline from 'pliny/ui/TOCInline'
+// import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
+
+import TOCInline from '@/components/blog/TOCInLine'
 import Image from '@/components/tools/Image'
 import CustomLink from '@/components/tools/Link'
 import TableWrapper from './TableWrapper'
